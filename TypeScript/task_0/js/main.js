c6ee4924e99b089661b7e09faedf2c35cd98ee1b
JoBEph <jordan.baroux31@gmail.com>
2025-05-26 11:46:52 +0200

    push main.js compiled by main.ts

---
+++ TypeScript/task_0/js/main.js
@@ -0,0 +1,15 @@
+var student1 = {
+    firstname: "Jo",
+    lastname: "holb",
+    age: 25,
+    location: "Toulouse",
+};
+var student2 = {
+    firstname: "Holbi",
+    lastname: "Bordx",
+    age: 17,
+    location: "Bordeaux",
+};
+
+console.log(`${student1.firstname} habite à ${student1.location}.`);
+console.log(`${student2.firstname} a ${student2.age} ans.`);
