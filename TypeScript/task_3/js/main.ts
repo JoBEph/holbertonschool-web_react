7b5690d0a4089bb3196f60d4134d213b465b55b0
JoBEph <jordan.baroux31@gmail.com>
2025-05-26 16:15:36 +0200

    Delete TypeScript/task_3/js directory

--- TypeScript/task_3/js/main.ts
+++
@@ -1,7 +0,0 @@
-function printTeacher(firstName: string, lastName: string): string {
-    const teacher = firstName.charAt(0);
-    return `${teacher}. ${lastName}`;
-}
-
-console.log(printTeacher("Joe", "TLS"));
-console.log(printTeacher("Catherine", "Johnson"));
