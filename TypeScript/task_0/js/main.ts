3435058cd53d712fdee2f3a3601f5145657d7794
JoBEph <jordan.baroux31@gmail.com>
2025-05-26 11:18:03 +0200

    push main task 0

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,18 @@
+interface studentsList  {
+    firstname : string,
+    lastname : string,
+    age : number,
+    location : string,
+}
+const student1: studentsList = {
+    firstname: "Jo",
+    lastname: "holb",
+    age: 25,
+    location: "Toulouse",
+};
+const student2: studentsList = {
+    firstname: "Holbi",
+    lastname: "Bordx",
+    age: 17,
+    location: "Bordeaux",
+};
