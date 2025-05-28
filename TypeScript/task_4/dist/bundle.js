97962ba194d54724bc02f40b04864a431dc5585a
JoBEph <jordan.baroux31@gmail.com>
2025-05-28 16:48:22 +0200

    feat: add Subject, Teacher, Cpp, Java, and React classes with respective methods

---
+++ TypeScript/task_4/dist/bundle.js
@@ -0,0 +1,22 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!********************************!*\
+  !*** ./js/subjects/Subject.ts ***!
+  \********************************/
+
+var Subjects;
+(function (Subjects) {
+    var Subject = /** @class */ (function () {
+        function Subject() {
+        }
+        Subject.prototype.setTeacher = function (teacher) {
+            this.teacher = teacher;
+        };
+        return Subject;
+    }());
+    Subjects.Subject = Subject;
+})(Subjects || (Subjects = {}));
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLElBQVUsUUFBUSxDQVFqQjtBQVJELFdBQVUsUUFBUTtJQUNoQjtRQUFBO1FBTUEsQ0FBQztRQUhDLDRCQUFVLEdBQVYsVUFBVyxPQUFnQjtZQUN6QixJQUFJLENBQUMsT0FBTyxHQUFHLE9BQU8sQ0FBQztRQUN6QixDQUFDO1FBQ0gsY0FBQztJQUFELENBQUM7SUFOWSxnQkFBTyxVQU1uQjtBQUNILENBQUMsRUFSUyxRQUFRLEtBQVIsUUFBUSxRQVFqQiIsInNvdXJjZXMiOlsid2VicGFjazovL3R5cGVzY3JpcHRfZGVwZW5kZW5jaWVzLy4vanMvc3ViamVjdHMvU3ViamVjdC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJuYW1lc3BhY2UgU3ViamVjdHMge1xuICBleHBvcnQgY2xhc3MgU3ViamVjdCB7XG4gICAgdGVhY2hlciE6IFRlYWNoZXI7XG5cbiAgICBzZXRUZWFjaGVyKHRlYWNoZXI6IFRlYWNoZXIpOiB2b2lkIHtcbiAgICAgIHRoaXMudGVhY2hlciA9IHRlYWNoZXI7XG4gICAgfVxuICB9XG59Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
